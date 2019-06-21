1e03ff5b803c2e80b7e5d6c155e5a9c9d205387d
Fankadore <rmackenzie88@gmail.com>
2019-06-21 17:40:16 +0100

    Changed Header from flex to grid

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './Header.scss';
-	
+
 function Header(props) {
 	const { title, logo } = props.content;
 
 	return (
 		<div className="header">
-			<div className="header__content">
-				<img src={logo} alt="logo" className="header__logo"/>
-				<h1 className="header__title">{title}</h1>
-			</div>
+			<img src={logo} alt="logo" className="header__logo" />
+			<h1 className="header__title">{title}</h1>
 		</div>
 	)
 }
